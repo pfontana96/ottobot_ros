4d37b9064efad61e244db9bb1011a11ceb87ff20
Pedro Fontana <pedro.fontana.1996@gmail.com>
2023-01-02 23:06:01 -0300

    Added compressed images for visualization

--- scripts/realsense_node.py
+++ scripts/realsense_node.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
 import pyrealsense2 as rs
+import numpy as np
 import yaml
 from  pathlib import Path
+import cv2
 
 from ottobot.rgbd_camera.realsensed435i import RealSenseD435i
 
@@ -22,6 +24,7 @@ class RealSenseCameraNode:
 
         self._frame_id = rospy.get_param("{}/frame_id".format(nodename), "map")
         self._frame_rate = rospy.get_param("{}/frame_rate".format(nodename), 15) # Defaults to 15 fps
+        self._compressed = rospy.get_param("{}/compress".format(nodename), False)
         self._ros_rate = rospy.Rate(self._frame_rate)
 
         self._calibration_filename = rospy.get_param("{}/calibration_filename".format(nodename), "camera_intrinsics")
@@ -31,6 +34,10 @@ class RealSenseCameraNode:
 
         self._rgb_pub = rospy.Publisher(color_topic, Image, queue_size=5)
         self._depth_pub = rospy.Publisher(depth_topic, Image, queue_size=5)
+
+        if self._compressed:
+            self._color_pub_compressed = rospy.Publisher("{}/compressed".format(color_topic), CompressedImage, queue_size=5)
+            self._depth_pub_compressed = rospy.Publisher("{}/compressed".format(depth_topic), CompressedImage, queue_size=5)
 
         self._height = 480
         self._width = 848
@@ -48,12 +55,13 @@ class RealSenseCameraNode:
         rospy.loginfo("DONE")
 
         self._camera = RealSenseD435i(ctx, self._frame_rate, self._height, self._width, dev_id)
+
+        self._cv_bridge = CvBridge()
     
     def run(self):
 
         try:
-            rospy.loginfo("here")
-            bridge = CvBridge()
+            rospy.loginfo("Starting streaming")
 
             # Get camera intrinsics and dump them in configuration file
             intrinsics = self._camera.intrinsics
@@ -86,26 +94,12 @@ class RealSenseCameraNode:
                     rospy.logerr("Either color frame or depth frame is None..")
                     continue
 
-                rospy.loginfo("depth: {} and color: {}".format(depth_image.shape, color_image.shape))
+                rospy.logdebug("depth: {} and color: {}".format(depth_image.shape, color_image.shape))
+                self._send_images(depth_image=depth_image, color_image=color_image, time=now)
 
-                # Convert images to ROS messages
-                try:
-                    color_image_msg = bridge.cv2_to_imgmsg(color_image, encoding="bgr8")
-                    depth_image_msg = bridge.cv2_to_imgmsg(depth_image, encoding="mono16")
-
-                except CvBridgeError as e:
-                    rospy.logerr(e)
-                    continue
-
-                color_image_msg.header.stamp = now
-                depth_image_msg.header.stamp = now
-
-                color_image_msg.header.frame_id = self._frame_id
-                color_image_msg.header.frame_id = self._frame_id
-
-
-                self._rgb_pub.publish(color_image_msg)
-                self._depth_pub.publish(depth_image_msg)
+                if self._compressed:
+                    # Compress images for visualization on Rviz on remote machine
+                    self._send_compressed(depth_image=depth_image, color_image=color_image, time=now)
 
                 # self._ros_rate.sleep()
 
@@ -114,6 +108,47 @@ class RealSenseCameraNode:
 
         finally:
             self._camera.shutdown()
+
+    def _send_images(self, depth_image: np.ndarray, color_image: np.ndarray, time: int):
+        # Convert images to ROS messages
+        try:
+            color_image_msg = self._cv_bridge.cv2_to_imgmsg(color_image, encoding="bgr8")
+            depth_image_msg = self._cv_bridge.cv2_to_imgmsg(depth_image, encoding="mono16")
+
+        except CvBridgeError as e:
+            rospy.logerr(e)
+            return
+
+        color_image_msg.header.stamp = time
+        depth_image_msg.header.stamp = time
+
+        color_image_msg.header.frame_id = self._frame_id
+        color_image_msg.header.frame_id = self._frame_id
+
+
+        self._rgb_pub.publish(color_image_msg)
+        self._depth_pub.publish(depth_image_msg)
+
+    def _send_compressed(self, depth_image: np.ndarray, color_image: np.ndarray, time: int):
+
+        # Apply color map to depth image for visualization
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        depth_image_msg = CompressedImage()
+        depth_image_msg.header.stamp = time
+        depth_image_msg.header.frame_id = self._frame_id
+        depth_image_msg.format = "jpeg"
+        depth_image_msg.data = np.array(cv2.imencode('.jpg', depth_colormap)[1]).tostring()
+
+        color_image_msg = CompressedImage()
+        color_image_msg.header.stamp = time
+        color_image_msg.header.frame_id = self._frame_id
+        color_image_msg.format = "jpeg"
+        color_image_msg.data = np.array(cv2.imencode('.jpg', color_image)[1]).tostring()
+
+        self._color_pub_compressed.publish(color_image_msg)
+        self._depth_pub_compressed.publish(depth_image_msg)
+
 
 if __name__ == "__main__":
 
